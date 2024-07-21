4425eb40792d2013e051d7bfa0d07529e528e350
Sai Yerni Akhil Madabattula <makhil123@yahoo.in>
2024-07-21 16:09:28 -0500

    Add author

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <pre>Hello, to the World from the Next.js App</pre>
+      <pre>By: <b> Sai Yerni Akhil Madabattula</b></pre>
     </main>
   );
 }
